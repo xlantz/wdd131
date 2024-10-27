fcbf39562e302b9b1d3567a745f4e5feb4b4b381
xlantz <xlanterman@gmail.com>
2024-10-26 18:14:04 -0600

    pretty much done

--- project/volcano-place.js
+++ project/volcano-place.js
@@ -14,3 +14,23 @@ const secondParagraph = footer.querySelector('p:nth-of-type(2)');
 // Update the content of the paragraphs
 firstParagraph.textContent = `© ${currentYear} | Alexander M. Lanterman | Volcano Place`;
 secondParagraph.textContent = `Last modified: ${lastModifiedDate}`;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const lazyImages = document.querySelectorAll("img.lazy");
+
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                img.src = img.dataset.src; 
+                img.classList.remove("lazy"); 
+                observer.unobserve(img);
+            }
+        });
+    });
+
+    lazyImages.forEach(image => {
+        imageObserver.observe(image);
+    });
+});
+
